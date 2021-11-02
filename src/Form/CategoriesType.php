a8bcca35d4d587a7062a42cb389bfab373635018
Bruno Richard <bruno683@outlook.fr>
2021-11-02 17:10:31 +0100

    Modified fields in CategoriesType

--- src/Form/CategoriesType.php
+++ src/Form/CategoriesType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Categories;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,16 @@ class CategoriesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
-            ->add('description')
+            ->add('Name', TextType::class, [
+                'label'=> 'Nom',
+                'choices'=> [
+                    'Blondes'=>'Blondes',
+                    'Malté'=>'Malté',
+                    'Brune'=>'Brunes',
+                    'Blanches'=>'blanches'
+                ]
+            ])
+            ->add('description', TextareaType::class)
         ;
     }
 
