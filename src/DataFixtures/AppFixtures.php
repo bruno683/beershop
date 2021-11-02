9d9331a0124cd2882668a93a10834ed827e221e6
Bruno Richard <bruno683@outlook.fr>
2021-11-02 13:25:39 +0100

    Add Appfixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Categories;
+use App\Entity\Products;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+       
+        //$product = new Products();
+        //$manager->persist($product);
+
+       
+        
+
 
         $manager->flush();
     }
