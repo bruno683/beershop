1a238581b5d98be7f841216b95b7ad493a1609ac
Bruno Richard <bruno683@outlook.fr>
2021-11-03 13:30:15 +0100

    Specified fields in productsCrudController and render categories to string

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -3,7 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProductsCrudController extends AbstractCrudController
 {
@@ -12,14 +20,35 @@ class ProductsCrudController extends AbstractCrudController
         return Products::class;
     }
 
-    /*
+    /**
+     * Configure the CRUD Products 
+     *
+     * @param Crud $crud
+     * @return Crud
+     */
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setEntityLabelInPlural('Produits')
+            ->setEntityLabelInSingular('Produit')
+            ->setSearchFields(['name', 'volume', 'categories.id'])
+            ->setDefaultSort(['name' => 'DESC']);
+    }
+
+    /**
+     * Configuration of the fields 
+     *
+     * @param string $pageName
+     * @return iterable
+     */
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            Field::new('name'),
+            Field::new('price'),
+            Field::new('ingredients'),
+            Field::new('volume'),
+            AssociationField::new('categories'),
         ];
     }
-    */
+    
 }
--- src/Entity/Categories.php
+++ src/Entity/Categories.php
@@ -97,4 +97,9 @@ class Categories
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
 }
