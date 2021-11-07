0f6f3006c95b3a20ee5150dd55d4e53de9f53a4c
Bruno Richard <bruno683@outlook.fr>
2021-11-07 16:32:10 +0100

    Modified ProductsCrudController

--- src/Controller/Admin/ProductsCrudController.php
+++ src/Controller/Admin/ProductsCrudController.php
@@ -46,7 +46,7 @@ class ProductsCrudController extends AbstractCrudController
     {
         return [
             Field::new('name'),
-            MoneyField::new('price')->setCurrency('EUR')->set,
+            MoneyField::new('price')->setCurrency('EUR'),
             Field::new('ingredients'),
             Field::new('volume'),
             TextField::new('imageFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
