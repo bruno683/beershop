1ddfb17ca87322de79694d408ff15db5eb55259f
Bruno Richard <bruno683@outlook.fr>
2021-11-07 10:35:58 +0100

    Added shop Controller and twig page

---
+++ src/Controller/ShopController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop', name: 'shop')]
+    public function index(ProductsRepository $productsRepository): Response
+    {
+        $items = $productsRepository->findAll(); 
+        return $this->render('shop/index.html.twig', [
+            'title' => 'Notre catalogue',
+            'items' => $items
+        ]);
+    }
+}
