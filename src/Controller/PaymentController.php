10cb79b62ef643e99d27b5c87f15ccdce6dc9aa1
Bruno Richard <bruno683@outlook.fr>
2021-11-08 15:07:52 +0100

    Add stripe checkout

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -31,22 +31,56 @@ class PaymentController extends AbstractController
             $totalItem = $item['produit']->getPrice() / 100 * $item['quantity'];
             $total += $totalItem;
         }
-        if (isset($_POST[$total]) && !empty($_POST[$total])) {
-            require_once('vendor/autoload.php');
-
-            \Stripe\Stripe::setApiKey($_ENV('STRIPE_SECRET_KEY'));
-
-            $intent = \Stripe\PaymentIntent::create([
-                'amount' => $total,
-                'currency' => 'eur'
-            ]);
-            dd($intent);
-        } 
         
         
-        return $this->render('payment/index.html.twig', [
+       return $this->render('payment/index.html.twig', [
             'title' => 'Paiement'
             
         ]);
+    }
+
+    /**
+     * @Route("/checkout", name = "payment_checkout")
+     */
+    public function checkout(Request $request, ProductsRepository $productRepo)
+    {
+        $session = $request->getSession();
+        $cart = $session->get('panier', []);
+        $cartWithData = [];
+
+        foreach ($cart as $id => $quantity) {
+            $cartWithData[] = [
+                'produit' => $productRepo->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        $total = 0;
+
+        foreach ($cartWithData as $item){
+            $totalItem = $item['produit']->getPrice() / 100 * $item['quantity'];
+            $total += $totalItem;
+        }
+        
+        \Stripe\Stripe::setApiKey('sk_test_51Js7GtL81yHRap8c46xs7Iu5XyBO4TRb7jxByndtFXLQvYCG6INLc3Ixlz3U0QCojWIp2gnoCoaf26hTyo7ejE4V00X29Wt8EO');
+
+        $stripeSession = \Stripe\Checkout\Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'eur',
+                'product_data' => [
+                  'name' => 'T-shirt',
+                ],
+                'unit_amount' => $total,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => 'https://example.com/success',
+            'cancel_url' => 'https://example.com/cancel',
+          ]);
+
+          dd($stripeSession);
     }
 }
