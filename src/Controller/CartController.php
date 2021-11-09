340f5fc644dfbaa7c7646b54a78480e1218235eb
Bruno Richard <bruno683@outlook.fr>
2021-11-09 10:26:37 +0100

    Clean use in CartController

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,16 +2,12 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Repository\CartRepository;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 
 class CartController extends AbstractController
